b95dd8e68145e436b6259149ab11d83523ce2ebd
ninjakid100 <feliciapace@abcs-imac.local>
2014-07-25 08:18:40 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Felicia Pace on 14.07.13.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
